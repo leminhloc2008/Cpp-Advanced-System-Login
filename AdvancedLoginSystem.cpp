f558245eae93a815e851293186b436da6f9c7c24
lmlstudio <77842401+lmlstudio@users.noreply.github.com>
2021-03-08 20:38:18 +0700

    Update AdvancedLoginSystem.cpp

--- AdvancedLoginSystem.cpp
+++ AdvancedLoginSystem.cpp
@@ -9,6 +9,7 @@
 #include <filesystem>
 using namespace std;
 
+//Change line color function, you don't need this if you don't like colorful line like me:v 
 void SetColor(int ForgC)
 {
     WORD wColor;
@@ -24,15 +25,18 @@ void SetColor(int ForgC)
 
 int main()
 {
+    //declare
     bool login_successful=false;
     long long option;
-    string account_name,account_name_login,password,password_login,username;
+    string account_name,account_name_login,password,password_login,username; 
     SetColor(14);
     cout<<"Welcome to my login system, made by LML STUDIO"<<endl<<"[Press 1] to sign up"<<endl<<"[Press 2] to login"<<endl<<"[Press 3] if you forgot your account password"<<endl;
     cout<<"Your Option: ";
     cin>>option;
+    //if option == 1 
     if(option==1)
     {
+        //insert information
         SetColor(11);
         cout<<"Create an user name: ";
         cin>>account_name;
@@ -40,10 +44,13 @@ int main()
         cout<<"Create a password: ";
         cin>>password;
         cout<<endl;
-        ofstream user_info;
-        user_info.open("user_"+account_name+".txt");
+        //file handling, create an user file
+        ofstream user_info; 
+        //create a file and insert data
+        user_info.open("user_"+account_name+".txt"); // file name = user_"account_name".txt
         user_info<<account_name<<endl<<password;
         user_info.close();
+        //after create file, close it
         SetColor(10);
         cout<<"Your account has been saved!"<<endl<<endl;
         main();
@@ -60,9 +67,12 @@ int main()
             cin>>account_name_login;
             cout<<endl<<"Type in your password: ";
             cin>>password_login;
+            //open created file, with the name same with the account name login
             ifstream find_account("user_"+account_name_login+".txt");
+            //find data
             getline(find_account,account_name);
             getline(find_account,password);
+            //if account_name_login and password_login is the same with information that we created before, cout<<"LOGGED IN";, else cout<<"Failed";
             if(account_name_login==account_name&&password_login==password)
             {
                 SetColor(2);
@@ -84,6 +94,7 @@ int main()
         SetColor(6);
         cout<<"Type in your username: ";
         cin>>username;
+        //find data file
         ifstream find_account("user_"+username+".txt");
         if(find_account.is_open())
         {
